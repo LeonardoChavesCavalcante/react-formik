3192d206cbe289f9cd1421e5ba0732d76c4b9826
LeonardoChavesCavalcante <leonardo.chavescavalcante@gmail.com>
2019-08-29 13:03:01 -0300

    projeto teste formik

--- src/PessoaFormik.tsx
+++ src/PessoaFormik.tsx
@@ -1,10 +1,19 @@
-import React, { FC, Fragment } from 'react';
+import React, { FC, Fragment, useState } from 'react';
 import { TPessoa } from './TPessoa';
 import { getPessoa } from './PessoaService';
 
 type Props = {
     title: string;
 }
+
+let initialPessoa: TPessoa = {
+    codigo: 1,
+    nome: 'Leonardo',
+    email: 'leonardo.chavescavalcante@gmail.com',
+    logradouro: 'Cocais'
+}
+
+const [pessoa, setPessoa] = useState<TPessoa>(initialPessoa);
 
 const PessoaFormik: FC<Props> = ({ title }) => {
 
